bb2fc58440e15eb74f249e70789473aacd4c6b72
Sid <sidharth.s.r@gmail.com>
2018-07-05 18:12:05 +0530

    Webpack config added

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import Actions from "./Actions";
+import Avatar from "./Avatar";
+import Bubble from "./Bubble";
+import Composer from "./Composer";
+import Day from "./Day";
+import GiftedAvatar from "./GiftedAvatar";
+import GiftedChat from "./GiftedChat";
+import GiftedChatInteractionManager from "./GiftedChatInteractionManager";
+import InputToolbar from "./InputToolbar";
+import LoadEarlier from "./LoadEarlier";
+import Message from "./Message";
+import MessageContainer from "./MessageContainer";
+import MessageImage from "./MessageImage";
+import MessageText from "./MessageText";
+import Send from "./Send";
+import Time from "./Time";
+import * as Util from "./utils";
+
+export {
+    Actions,
+    Avatar,
+    Bubble,
+    Composer,
+    Day,
+    GiftedAvatar,
+    GiftedChat,
+    GiftedChatInteractionManager,
+    InputToolbar,
+    LoadEarlier,
+    Message,
+    MessageContainer,
+    MessageImage,
+    MessageText,
+    Send,
+    Time,
+    Util
+};
+
+// export * from './src/GiftedChat';
